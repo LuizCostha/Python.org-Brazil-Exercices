5bb9c51901eb254f63636224a86b3e399386f209
LuizCostha <95573660+LuizCostha@users.noreply.github.com>
2021-12-06 11:10:16 -0300

    new exercise

---
+++ PYORG ex 2.2.py
@@ -0,0 +1,7 @@
+n = float(input("Digite um número real: "))
+if n == 0:
+    print ("O número digitado é zero.")
+elif n > 0:
+    print ("O número digitado é positivo.")
+elif n < 0:
+    print("O número digitado é negativo.")
