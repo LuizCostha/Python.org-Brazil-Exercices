80eeb2131aff5a67b667370c549fbdf9d02f827c
LuizCostha <95573660+LuizCostha@users.noreply.github.com>
2021-12-06 11:16:53 -0300

    new exercises

---
+++ PYORG ex 2.3.py
@@ -0,0 +1,7 @@
+sex = input("Digite M para masculino e F para feminino: ")
+if sex == "M":
+    print ("M - Masculino")
+elif sex == "F":
+    print ("F - Feminino")
+else:
+    print ("Sexo inválido")
