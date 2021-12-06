c1f599a45b9c972bf2db4a750510f8c71fa0d2cd
LuizCostha <95573660+LuizCostha@users.noreply.github.com>
2021-12-06 11:04:08 -0300

    new exercise 2.1

---
+++ PYORG ex 2.1.py
@@ -0,0 +1,6 @@
+n1 = float(input("Digite o primeiro número que pretende comparar: "))
+n2 = float(input("Digite o segundo número que pretende comparar: "))
+if n1 > n2:
+    print ("O maior número é: %5.5f." % n1)
+else:
+    print ("O maior número é: %5.5f." % n2)
