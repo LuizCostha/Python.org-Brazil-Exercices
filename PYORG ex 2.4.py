dfeb36e1e9f49d724bfc712e8379071394cd102c
LuizCostha <95573660+LuizCostha@users.noreply.github.com>
2021-12-10 12:09:30 -0300

    Exercício 2.4
    Faça um Programa que verifique se uma letra digitada é vogal ou consoante.

---
+++ PYORG ex 2.4.py
@@ -0,0 +1,6 @@
+letra = input("Digite uma letra: ")
+if letra == "a" or letra == "A" or letra == "e"  or letra == "E" or letra == "i" or letra == "I" or letra == "o" or letra == "O" or letra == "u" or letra == "U":
+    print("A letra %s é uma vogal. " % letra)
+else:
+    print ("A letra %s é uma consoante. " % letra)
+
