836fe62e2a66cfbd50b7df4787d470553745d3e7
LuizCostha <95573660+LuizCostha@users.noreply.github.com>
2021-12-11 15:32:16 -0300

    Exercício 2.13
    Faça um Programa que leia um número e exiba o dia correspondente da semana. (1-Domingo, 2- Segunda, etc.), se digitar outro valor deve aparecer valor inválido.

---
+++ PYORG ex 2.13.py
@@ -0,0 +1,17 @@
+numero = int(input("Digite o dia da semana desejado: "))
+if numero == 1:
+    print("O dia da semana correspondente é Domingo. ")
+elif numero == 2:
+    print("O dia da semana correspondente é Segunda-feira. ")
+elif numero == 3:
+    print("O dia da semana correspondente é Terça-feira. ")
+elif numero == 4:
+    print("O dia da semana correspondente é Quarta-feira. ")
+elif numero == 5:
+    print("O dia da semana correspondente é Quinta-feira. ")
+elif numero == 6:
+    print("O dia da semana correspondente é Sexta-feira. ")
+elif numero == 7:
+    print("O dia da semana correspondente é Sábado. ")
+else:
+    print ("Valor inválido! ")
